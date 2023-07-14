d88afda226d9ecd832cf892107fa8e73485c6a3e
luishsilva <luizhrick@gmail.com>
2023-07-14 00:39:38 -0700

    Add eslint and pretter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,11 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    semi: ['warn', 'always'],
+    eqeqeq: 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
