d492ad52a4314f733aabc75b85197746921b146b
luishsilva <luizhrick@gmail.com>
2023-07-21 08:19:32 -0700

    Refactor imports component names

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Header } from './components/header/Header';
-import CharactersTable from './components/characters-table/CharactersTable';
-import { Cards } from './components/cards/Cards';
+//import CharactersTable from './components/characters-table/CharactersTable';
+//import Table from './components/characters-table/Table';
+import Table from './components/characters-table/Table';
+import  { Cards }  from './components/cards/Cards';
 import { data } from './fma-data';
 
 function App() {
   return (
     <>
       <Header />
-      <CharactersTable items={data}/>
-      <Cards items={data}/>
+      <Table characters={data}/>
+      <Cards characters={data}/>
     </>
   );
 }
